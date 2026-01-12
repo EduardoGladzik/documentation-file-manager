04777fdf2fb0b049a233cbecf92840a7019e0d73
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-01-12 13:14:54 -0300

    Feature: create a copy of the checklist template in the rat folder.

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ import shutil
 img_extensions = ['.jpg', '.jpeg', '.png', '.jfif']
 DESKTOP_PATH = Path.home() / "Desktop"
 CLIENTS_FOLDER = DESKTOP_PATH / "Clientes"
+CHECKLIST_PATH = DESKTOP_PATH / "checklist.txt"
 
 
 def find_client_folder(client_name):
@@ -41,6 +42,18 @@ def move_images_to_photos_folder(rat_folder):
                 print(f"Erro ao mover arquivos: {e}")
 
 
+def copy_template_checklist(folder_name, rat_path):
+    """
+    Copies the checklist template to the RAT folder.
+    """
+    new_file_name = folder_name
+    target_path = rat_path / "checklist.txt"
+    try:
+        shutil.copy(str(CHECKLIST_PATH), str(target_path))
+    except Exception as e:
+        print(f"Erro ao copiar o checklist: {e}")
+
+
 def main():
     client_input = input("Insira o nome do cliente: ")
     client_path = find_client_folder(client_input)
@@ -51,6 +64,8 @@ def main():
         rat_path = client_path / folder_name
         move_images_to_photos_folder(rat_path)
         print("Imagens movidas com sucesso para a pasta 'Fotos'.")
+        copy_template_checklist(folder_name, rat_path)
+        print("Checklist copiado com sucesso.")
     else:
         print(f"Erro: O cliente '{client_input}' não foi encontrado em 'Avulsos' ou 'Contratados'.")
         print("Verifique se o nome está correto ou se a pasta do cliente existe.")        
